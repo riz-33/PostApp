d3a7fe456a6ab3a60af9858675c25ddabef5f25a
rizwan <118995669+riz-33@users.noreply.github.com>
2024-05-29 02:46:58 +0500

    update

--- signup.js
+++ signup.js
@@ -1,4 +1,5 @@
 function registrationForm() {
+    event.preventDefault();
     var firstName = document.getElementById('firstName');
     var lastName = document.getElementById('lastName');
     var email = document.getElementById('email');
@@ -6,24 +7,84 @@ function registrationForm() {
     var password = document.getElementById('password');
     var confirmPassword = document.getElementById('confirmPassword');
 
-    var storage = localStorage.getItem("userData");
-    var arr = JSON.parse(storage);
-
-    if (!arr) {
-        arr = []
+    if (firstName.value.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Please Enter Your First Name",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
     }
-
-    var userData = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        number: number.value,
-        password: password.value,
-        confirmPassword: confirmPassword.value,
+    else if (lastName.value.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Please Enter Your Last Name",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
     }
+    else if (email.value.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Please Enter Your Email",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
+    }
+    else if (number.value.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Please Enter Your Contact Number",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
+    }
+    else if (password.value.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Please Enter Password",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
+    }
+    else if (password.value !== confirmPassword.value) {
+        Swal.fire({
+            icon: "error",
+            title: "Password Not Match",
+            // text: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        })
+    }
+    else {
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${firstName.value + " " + lastName.value} You're Successfully Registered`,
+            showConfirmButton: false,
+            timer: 1500
+        });
 
-    arr.push(userData)
-    localStorage.setItem("userData", JSON.stringify(arr))
+        var storage = localStorage.getItem("userData");
+        var arr = JSON.parse(storage);
 
-    // window.location.href = "/login.html"
+        if (!arr) {
+            arr = []
+        }
+
+        var userData = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            number: number.value,
+            password: password.value,
+            confirmPassword: confirmPassword.value,
+        }
+
+        arr.push(userData)
+        localStorage.setItem("userData", JSON.stringify(arr))
+
+        setTimeout(() => {
+            window.location.href = "/login.html"
+        }, 2000)
+    }
 }
